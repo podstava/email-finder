3c466b9d76828bdf7a2f47bb9bcaf5f6d9377910
Vyacheslav Krachkov <vskrachkov@gmail.com>
2016-08-29 17:34:21 +0300

    threads added

---
+++ email_finder.py
@@ -0,0 +1,181 @@
+import os
+import argparse
+import logging
+
+import timeit
+from validate_email import validate_email
+from urlparse import urlparse
+# from google import search as google_search
+import csv
+
+from selenium import webdriver, common
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(levelname)s] - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def google_search(company_name):
+    logger.info('starting search for domain of {}'.format(company_name))
+    driver = webdriver.Firefox()
+    driver.get("http://www.google.com")
+    input_element = driver.find_element_by_name("q")
+    try:
+        input_element.send_keys(company_name)
+        input_element.submit()
+
+        RESULTS_LOCATOR = "//div/cite"
+
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, RESULTS_LOCATOR)))
+
+        page1_results = driver.find_elements(By.XPATH, RESULTS_LOCATOR)
+        res_list = []
+        for item in page1_results[0:4]:
+            logger.info('url found {}'.format(item.text))
+            res_list += [item.text]
+        driver.close()
+        return res_list
+    except common.exceptions.TimeoutException:
+        logger.info('got timeout')
+        driver.close()
+        driver = webdriver.Firefox()
+        driver.get("http://www.google.com")
+
+
+SUPPORTED_FILE_TYPES = ['.csv']
+
+
+def csv_reader(filepath):
+    with open(filepath, 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            yield row
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help='Path to data.', required=True)
+    args = parser.parse_args()
+
+    if os.path.exists(args.file):
+        file_name, file_ext = os.path.splitext(args.file)
+        if not file_ext:
+            logger.error('File without extension.')
+            return
+        if file_ext not in SUPPORTED_FILE_TYPES:
+            logger.error('File type not supported.')
+            return
+
+        counter = 0
+        done = 0
+        limit = 100
+        start = timeit.default_timer()
+        for row in csv_reader(args.file):
+            if counter == limit:
+                break
+            try:
+                name, lastname = row[0].lower().split(' ')
+                domain = parse_domains(row[1])
+                done += 1 if validate(make_variations(name, lastname, domain)) else 0
+                percents = (counter * 100) / limit
+                logger.info('{:.2f}% processed.'.format(percents))
+
+            except ValueError:
+                logger.info('name contains more than two words')
+                pass
+
+            counter += 1
+        logger.info('{}/{} emails found'.format(done, counter))
+        logger.info('finished for {}'.format(timeit.default_timer() - start))
+
+    else:
+        logger.error('File doesn\'t exist.')
+        return
+
+stop_domains = ['linkedin.com',
+                'twitter.com',
+                'facebook.com',
+                'vk.com',
+                'microsoft.com',
+                'wikipedia.com',
+                'tripadvisor.com',
+                'youtube.com',
+                'ru - ru.facebook.com']
+def parse_domains(company_name):
+    logger.info('company: {}'.format(company_name))
+    if 'freelance' in company_name.lower() or 'google' in company_name.lower() or company_name == '' or company_name == '-':
+        return ['gmail.com']
+    domains = google_search(company_name)
+    res_list = []
+    for domain in domains:
+        if urlparse(domain)[1] == '':
+            domain = urlparse(domain)[2]
+        else:
+            domain = urlparse(domain)[1]
+        if 'www.' in domain:
+            domain = domain[4:]
+        if '/' in domain:
+            domain = domain[:domain.index('/')]
+        if '\\' in domain:
+            domain = domain[:domain.index('\\')]
+        if domain not in stop_domains:
+            res_list += [domain]
+    res_list += ['gmail.com']
+    return res_list
+
+
+def make_variations(fname, lname, domains):
+    logger.info('-------- start making variations --------')
+    try:
+        fchar = fname[0]
+        lchar = lname[0]
+    except IndexError:
+        logger.error('index error')
+        return
+    emails = []
+    try:
+        for domain in domains:
+            emails += ['{}@{}'.format(fname, domain)]
+            emails += ['{}{}@{}'.format(fname, lname, domain)]
+            emails += ['{}_{}@{}'.format(fname, lname, domain)]
+            emails += ['{}.{}@{}'.format(fname, lname, domain)]
+            emails += ['{}{}@{}'.format(fchar, lname, domain)]
+            emails += ['{}_{}@{}'.format(fchar, lname, domain)]
+            emails += ['{}.{}@{}'.format(fchar, lname, domain)]
+            emails += ['{}{}@{}'.format(fname, lchar, domain)]
+            emails += ['{}_{}@{}'.format(fname, lchar, domain)]
+            emails += ['{}.{}@{}'.format(fname, lchar, domain)]
+            emails += ['{}{}@{}'.format(fchar, lchar, domain)]
+    except UnicodeDecodeError:
+        logger.error('Name contains specific symbols')
+        return
+    logger.info('variations for {} {}'.format(fname, lname))
+    for email in emails:
+        logger.info(email)
+    logger.info('-------- end variations --------')
+    return emails
+
+
+def validate(emails):
+    logger.info('-------- validation started --------')
+    if emails:
+        for email in emails:
+            if validate_email(email, verify=True):
+                logger.info('*** valid email: {} ***'.format(email))
+                logger.info('-------- validation ended --------')
+                return email
+            logger.info('invalid email: {}'.format(email))
+    return None
+
+
+if __name__ == '__main__':
+    main()
---
+++ search.py
@@ -0,0 +1,194 @@
+import os
+import logging
+
+import timeit
+from urlparse import urlparse
+
+import csv
+
+from validate_email import validate_email
+
+from selenium import webdriver, common
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from threading import Thread
+from Queue import Queue as Q
+import Queue
+
+
+FILE = '/home/vs/Downloads/attendees.csv'
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(levelname)s] - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def google_search(company_name):
+    logger.info('starting search for domain of {}'.format(company_name))
+    driver = webdriver.Firefox()
+    driver.get("http://www.google.com")
+    input_element = driver.find_element_by_name("q")
+    try:
+        input_element.send_keys(company_name)
+        input_element.submit()
+
+        result_location = "//div/cite"
+
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, result_location)))
+
+        page1_results = driver.find_elements(By.XPATH, result_location)
+        res_list = []
+        for item in page1_results[0:4]:
+            logger.info('url found {}'.format(item.text))
+            res_list += [item.text]
+        driver.close()
+        return res_list
+    except common.exceptions.TimeoutException:
+        logger.info('got timeout')
+        driver.close()
+        driver = webdriver.Firefox()
+        driver.get("http://www.google.com")
+
+
+def csv_reader(file_path):
+    with open(file_path, 'rb') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            yield row
+
+
+def parse_domains(company_name):
+    stop_domains = ['linkedin.com',
+                    'twitter.com',
+                    'facebook.com',
+                    'vk.com',
+                    'microsoft.com',
+                    'wikipedia.com',
+                    'tripadvisor.com',
+                    'youtube.com',
+                    'ru - ru.facebook.com']
+
+    logger.info('company: {}'.format(company_name))
+    if 'freelance' in company_name.lower() or 'google' in company_name.lower() or company_name == '' or company_name == '-':
+        return ['gmail.com']
+    domains = google_search(company_name)
+    res_list = []
+    for domain in domains:
+        if urlparse(domain)[1] == '':
+            domain = urlparse(domain)[2]
+        else:
+            domain = urlparse(domain)[1]
+        if 'www.' in domain:
+            domain = domain[4:]
+        if '/' in domain:
+            domain = domain[:domain.index('/')]
+        if '\\' in domain:
+            domain = domain[:domain.index('\\')]
+        if domain not in stop_domains:
+            res_list += [domain]
+    res_list += ['gmail.com']
+    return res_list
+
+
+def make_variations(first_name, last_name, domains):
+    logger.info('-------- start making variations --------')
+    try:
+        first_char = first_name[0]
+        last_char = last_name[0]
+    except IndexError:
+        logger.error('index error')
+        return
+    emails = []
+    try:
+        for domain in domains:
+            emails += ['{}@{}'.format(first_name, domain)]
+            emails += ['{}{}@{}'.format(first_name, last_name, domain)]
+            emails += ['{}_{}@{}'.format(first_name, last_name, domain)]
+            emails += ['{}.{}@{}'.format(first_name, last_name, domain)]
+            emails += ['{}{}@{}'.format(first_char, last_name, domain)]
+            emails += ['{}_{}@{}'.format(first_char, last_name, domain)]
+            emails += ['{}.{}@{}'.format(first_char, last_name, domain)]
+            emails += ['{}{}@{}'.format(first_name, last_char, domain)]
+            emails += ['{}_{}@{}'.format(first_name, last_char, domain)]
+            emails += ['{}.{}@{}'.format(first_name, last_char, domain)]
+            emails += ['{}{}@{}'.format(first_char, last_char, domain)]
+    except UnicodeDecodeError:
+        logger.error('Name contains specific symbols')
+        return
+    logger.info('variations for {} {}'.format(first_name, last_name))
+    for email in emails:
+        logger.info(email)
+    logger.info('-------- end variations --------')
+    return emails
+
+
+def validate(emails):
+    logger.info('-------- validation started --------')
+    if emails:
+        for email in emails:
+            if validate_email(email, verify=True):
+                logger.info('*** valid email: {} ***'.format(email))
+                logger.info('-------- validation ended --------')
+                return email
+            logger.info('invalid email: {}'.format(email))
+    return None
+
+
+def handler_thread():
+    while True:
+        try:
+            row = q_initial.get(block=False)
+            name, last_name = row[0].lower().split(' ')
+            domain = parse_domains(row[1])
+            # done += 1 if validate(make_variations(name, last_name, domain)) else 0
+            # percents = (counter * 100) / limit
+            # logger.info('{:.2f}% processed.'.format(percents))
+        except Queue.Empty:
+            break
+        except ValueError:
+            logger.info('name contains more than two words')
+
+
+def writer_thread():
+    """Write output to file here."""
+    while True:
+        try:
+            print q_result.get()
+        except Queue.Empty:
+            break
+
+
+q_initial = Q()
+q_result = Q()
+
+
+def threads_start(file_reader, csv_file, threads_count):
+    for row in file_reader(csv_file):
+        q_initial.put(row)
+
+    threads = []
+
+    for i in xrange(threads_count):
+        th = Thread(target=handler_thread)
+        th.start()
+        threads.append(th)
+
+        tw = Thread(target=writer_thread)
+        tw.start()
+        threads.append(tw)
+
+    for t in threads:
+        t.join()
+        print t
+        print threads
+
+
+if __name__ == '__main__':
+    threads_start(csv_reader, FILE, 8)
